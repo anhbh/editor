8901cab4640e7cd2a27bde4cd9738ec1bfaecd16
Hoang Anh <anhbh.253@gmail.com>
2022-12-30 10:53:14 +0700

    03- Install cscope_maps.vim

--- install.sh
+++ install.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+### preparation
+#sudo apt-get update
+#sudo apt-get install -y vim cscope ctags
 
 # directory
 mkdir -p ~/.vim
+mkdir -p ~/.local/bin
 
 
 ### install files
-## vimrc
+## .vimrc
 cp files/vimrc ~/.vimrc
 
-
-## vim
+## .vim
 cp -r files/vim ~/.vim
+
+## csbuild
+cp files/csbuild ~/.local/bin/
+
+
 
 
 ## plugins
 # plugin manager vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# cscope_maps.vim
+git clone https://github.com/joe-skb7/cscope-maps.git ~/.vim/plugin/cscope_maps.vim
 
 ### install plugins
 vim -c PluginInstall -c qa
